923a3156dc7182eb96b3e15121cce8ef9dd0a56b
Evan Cameron <evan@vectorface.com>
2014-12-08 12:46:57 -0500

    secure RNG, hmac, md5 and sha256

--- src/rust_mnemonic.rs
+++ src/rust_mnemonic.rs
@@ -1,19 +1,50 @@
 extern crate "rust-crypto" as rust_crypto;
 
-use rust_crypto::sha2::{Sha512, Sha384, Sha512Trunc256, Sha512Trunc224, Sha256, Sha224};
+use std::io::IoResult;
+use std::num::{Int, ToPrimitive};
+use std::rand::{OsRng, Rng};
+
+
+use rust_crypto::pbkdf2::pbkdf2;
+use rust_crypto::sha2::Sha256;
 use rust_crypto::digest::Digest;
 use rust_crypto::md5::Md5;
+use rust_crypto::mac::Mac;
+use rust_crypto::hmac::Hmac;
+
+
+pub struct Mnemonic {
+    words: Vec<u8>
+}
 
 
 fn main() {
   let mut sh = Md5::new();
   sh.input_str("The quick brown fox jumps over the lazy dog");
   let out_str = sh.result_str();
-  println!("{}",out_str);
+  //println!("{}",out_str);
+
+  // let mut rng = rand::task_rng();
+  // if rng.gen() {
+  //     println!("int: {}, uint: {}", rng.gen::<int>(), rng.gen::<uint>())
+  // }
+
+  // let password = "The quick brown fox jumps over the lazy dog";
+  // let mac = Hmac::new(Sha256::new(), password.as_bytes());
+  // let stuff = String::from_utf8(mac.to_vec());
+  // println!("{}",stuff);
+
+  let mut rng = match OsRng::new() {
+      Ok(g) => g,
+      Err(e) => panic!("Failed to obtain OS RNG: {}", e)
+  };
+
+  let num:u32 = rng.next_u32();
+  println!("{}",num);
 
 
-  let mut sh = Sha512::new();
+  let mut sh = Sha256::new();
   sh.input_str("");
   let out_str = sh.result_str();
-  println!("{}",out_str);
+  //println!("{}",out_str);
 }
